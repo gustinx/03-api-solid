3f133e5a661bd881fa02ca17197fe72e4bda48a2
Mateus <mateusbueno382@gmail.com>
2023-07-17 14:10:30 -0300

    initial commit

---
+++ src/http/controllers/gyms/search.spec.ts
@@ -0,0 +1,55 @@
+import request from 'supertest'
+import { app } from '@/app'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+import { createAndAuthenticateUser } from '@/utils/test/create-and-authenticate-user'
+
+describe('Search Gyms (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to search a gym', async () => {
+    const { token } = await createAndAuthenticateUser(app, true)
+
+    await request(app.server)
+      .post('/gyms')
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        title: 'Test gym',
+        description: 'test description',
+        phone: '1234567890',
+        latitude: -27.2092052,
+        longitude: -49.6401091,
+      })
+
+    await request(app.server)
+      .post('/gyms')
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        title: 'Example gym',
+        description: 'test description',
+        phone: '1234567890',
+        latitude: -27.2092052,
+        longitude: -49.6401091,
+      })
+
+    const response = await request(app.server)
+      .get('/gyms/search')
+      .query({
+        q: 'Example',
+      })
+      .set('Authorization', `Bearer ${token}`)
+      .send()
+
+    expect(response.statusCode).toEqual(200)
+    expect(response.body.gyms).toHaveLength(1)
+    expect(response.body.gyms).toEqual([
+      expect.objectContaining({
+        title: 'Example gym',
+      }),
+    ])
+  })
+})
